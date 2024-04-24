fc4ae1e40e2cd6e28c5426712cb33e88a370e6ec
Pradyun Pawar <140912655+pradyun-pawar@users.noreply.github.com>
2024-04-24 12:03:40 +0530

    Time: 113 ms (75.66%) | Memory: 54.5 MB (92.93%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -1,29 +1,40 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if(head==NULL || head->next==NULL) return head;
-        ListNode* temp=head;
-        vector<int> storage;
-        while(temp!=NULL){
-            storage.push_back(temp->val);
-            temp=temp->next;
+        if (!head || !head->next) return head;
+        ListNode* mid = findMiddle(head);
+        ListNode* right = sortList(mid->next);
+        mid->next = nullptr; // Splitting the list into two halves
+        ListNode* left = sortList(head);
+        return merge(left, right);
+    }
+    
+    // Function to find the middle node of the linked list
+    ListNode* findMiddle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        sort(storage.begin(),storage.end());
-        temp=head;
-        for(int i=0;i<storage.size();i++){
-            temp->val=storage[i];
-            temp=temp->next;
+        return slow;
+    }
+    
+    // Function to merge two sorted linked lists
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
         }
-        return head;
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
     }
 };
