314396f3dd6c4278c7bc06b5a4deebb2204a1ee4
Pradyun Pawar <140912655+pradyun-pawar@users.noreply.github.com>
2024-06-17 10:36:25 +0530

    Time: 3 ms (57.89%) | Memory: 8.1 MB (66.95%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -4,7 +4,7 @@ public:
         bool keep = false;
         bool keep2 = false;
         int ans = 0;
-        long long result = 0; // Use long long to handle overflow
+        long long result = 0; 
         int count = 0;
 
         for (int i = 0; i < s.size(); i++) {
@@ -17,13 +17,11 @@ public:
             else if(s[i]=='-' && s[i+1]=='+' || s[i]=='+' && s[i+1]=='-') return 0;
             else if (s[i] == '-' && keep2 == false) {
                 keep = true;
-                keep2 = true; // Ensure keep2 is set to true to avoid continuous
-                              // '-' processing
+                keep2 = true; 
                 continue;
             } else if ((s[i] == '-'||s[i] =='+') && keep2 == true) {
                 break;
-            } else if (s[i] == '+' &&
-                       keep2 == false) { // Handle the case of leading '+'
+            } else if (s[i] == '+' && keep2 == false) {
                 keep2 = true;
                 continue;
             } else if (isdigit(s[i]) && count < 11) {
@@ -35,16 +33,12 @@ public:
                 result = result + ans;
                 count++;
 
-                // Check for overflow conditions and round appropriately
                 if (result > INT_MAX) {
                     return keep ? INT_MIN : INT_MAX;
                 }
-            } else if (count == 11 &&
-                       keep == true) { // Fix typo: use '==' instead of '='
+            } else if (count == 11 && keep == true) { 
                 return INT_MIN;
-            } else if (count == 11 &&
-                       keep ==
-                           false) { // Handle the overflow for positive numbers
+            } else if (count == 11 && keep == false) { 
                 return INT_MAX;
             } else if (isalpha(s[i])) {
                 break;
@@ -54,7 +48,6 @@ public:
             result = -result;
         }
 
-        // Ensure result is within the 32-bit signed integer range
         if (result < INT_MIN)
             return INT_MIN;
         if (result > INT_MAX)
