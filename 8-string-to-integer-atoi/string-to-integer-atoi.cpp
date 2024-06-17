daad23f59f911be64e27d8da0955f1014ece67f1
Pradyun Pawar <140912655+pradyun-pawar@users.noreply.github.com>
2024-06-17 10:39:26 +0530

    Time: 3 ms (57.89%) | Memory: 8 MB (98.89%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -5,7 +5,6 @@ public:
         bool keep2 = false;
         int ans = 0;
         long long result = 0; 
-        int count = 0;
 
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == ' ' && keep2 == false)
@@ -24,22 +23,18 @@ public:
             } else if (s[i] == '+' && keep2 == false) {
                 keep2 = true;
                 continue;
-            } else if (isdigit(s[i]) && count < 11) {
+            } else if (isdigit(s[i])) {
                 result = result * 10;
                 ans = s[i] - '0';
                 keep2 = true;
                 if (ans == 0 && result == 0)
                     continue;
                 result = result + ans;
-                count++;
 
                 if (result > INT_MAX) {
                     return keep ? INT_MIN : INT_MAX;
                 }
-            } else if (count == 11 && keep == true) { 
-                return INT_MIN;
-            } else if (count == 11 && keep == false) { 
-                return INT_MAX;
+            
             } else if (isalpha(s[i])) {
                 break;
             }
