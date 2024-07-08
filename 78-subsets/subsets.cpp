ee20ac7298eedff5a3e72d56ba0aa944de482ff1
Pradyun Pawar <140912655+pradyun-pawar@users.noreply.github.com>
2024-07-08 22:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (77.84%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> saloni(int index,vector<int> &nums,vector<int> &result){
-        vector<vector<int>> final;
-        if(index >= nums.size()){
-            final.push_back(result);
-            return final;
+    void saloni(int index, vector<int>&nums, vector<int>&current, vector<vector<int>>&result){
+        if(index==nums.size()){
+            result.push_back(current);
+            return;
         }
-        result.push_back(nums[index]);
-        vector<vector<int>>include=saloni(index + 1,nums,result);
-        final.insert(final.end(), include.begin(), include.end());
-
-        result.pop_back();
-        vector<vector<int>>exclude=saloni(index + 1,nums,result);
-        final.insert(final.end(), exclude.begin(), exclude.end());
-        return final;
+        //include
+        current.push_back(nums[index]);
+        saloni(index+1,nums,current,result);
+        //exclude
+        current.pop_back();
+        saloni(index+1,nums,current,result);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        if(nums.size() == 0) return {};
-        vector<int> result;
-        return saloni(0,nums,result);
+        if(nums.size()==0) return {};
+        vector<int> current;
+        vector<vector<int>> result;
+        int index=0;
+
+        saloni(index,nums,current,result);
+        return result;
     }
 };
