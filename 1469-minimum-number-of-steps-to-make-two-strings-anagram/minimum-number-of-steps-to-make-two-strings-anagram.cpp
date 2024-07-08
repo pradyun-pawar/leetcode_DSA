14b307daef4c8ba3fa3a11e854bd162d827b4a52
Pradyun Pawar <140912655+pradyun-pawar@users.noreply.github.com>
2024-07-08 18:48:44 +0530

    Time: 137 ms (5.25%) | Memory: 19.1 MB (12.76%) - LeetSync

---
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        map<char,int> mpp;
+        int answer=0;
+
+        for(int i=0;i<s.size();i++){
+            mpp[s[i]]++;
+        }
+        auto it=mpp.begin();
+        for(int i=0;i<t.size();i++){
+            while(it!=mpp.end()){
+                if(it->first==t[i]){
+                    if(it->second >= 1){
+                        it->second--;
+                        break;
+                    }
+                    else{
+                        answer++;
+                        break;
+                    }
+                }
+                
+                // else if(it->first > t[i]){
+                //     answer++;
+                //     break;
+                // }
+                it++;
+            }
+            if(it==mpp.end()){
+                answer++;
+            }
+            it=mpp.begin();
+        }
+        return answer;
+    }
+};
