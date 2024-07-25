84c69b25d5877a57f006614a2088bfc9e1d504c4
Pradyun Pawar <140912655+pradyun-pawar@users.noreply.github.com>
2024-07-26 00:05:21 +0530

    Time: 6 ms (17.08%) | Memory: 12.7 MB (27.64%) - LeetSync

---
+++ 986-largest-time-for-given-digits/largest-time-for-given-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    std::string largestTimeFromDigits(std::vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        std::string answer = "";
+
+        do {
+            int hours = arr[0] * 10 + arr[1];
+            int minutes = arr[2] * 10 + arr[3];
+            if (hours < 24 && minutes < 60) {
+                std::string current = std::to_string(arr[0]) + std::to_string(arr[1]) + ':' + std::to_string(arr[2]) + std::to_string(arr[3]);
+                if (current > answer) {
+                    answer = current;
+                }
+            }
+        } while (next_permutation(arr.begin(), arr.end()));
+
+        return answer;
+    }
+};
